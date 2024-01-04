e740acce0205ca02c6d43fc326a80142dca758f5
Azwar <azwarnaim97@gmail.com>
2024-01-04 11:13:47 +0800

    @Update

--- components/storyblok/campaignProfile.js
+++ components/storyblok/campaignProfile.js
@@ -1,12 +1,9 @@
 "use client";
-
-import Link from "next/link";
 import Tab from "../tab";
 import { useRouter } from "next/navigation";
-
-function classNames(...classes) {
-  return classes.filter(Boolean).join(" ");
-}
+import useSWR from "swr";
+import { fetcher } from "helper/common";
+import { useEffect, useState } from "react";
 const tabs = [
   { name: "Details", href: "#", current: true, disable: false },
   { name: "Updates", href: "#", current: false, disable: false },
@@ -15,13 +12,81 @@ const tabs = [
   { name: "Community", href: "#", current: false, disable: true },
 ];
 const CampaignProfile = ({ data, generalContent }) => {
+  const [collPercentage, setcollPercentage] = useState(0);
   const router = useRouter();
-
-  console.log("campaign general:", data.story.content.cover_image.filename);
+  // console.log("campaign general:", data.story.content.cover_image.filename);
+  console.log("campaign: ", data);
   let c_data = data?.story;
   const handleClick = (e) => {
     router.push("/checkout");
   };
+
+  const { data: responseStat, error } = useSWR(
+    !!data?.story?.content
+      ? `${process.env.NEXT_PUBLIC_API_BASE_URL}v1/campaign/${data?.story?.content?.code}`
+      : null,
+    fetcher
+  );
+  console.log("responseStat: ", responseStat);
+
+  function dateFormat(date, type) {
+    let monthList = [
+      { value: "01", name: "Jan" },
+      { value: "02", name: "Feb  " },
+      { value: "03", name: "Mar" },
+      { value: "04", name: "Apr" },
+      { value: "05", name: "May" },
+      { value: "06", name: "June" },
+      { value: "07", name: "July" },
+      { value: "08", name: "Aug" },
+      { value: "09", name: "Sept" },
+      { value: "10", name: "Oct" },
+      { value: "11", name: "Nov" },
+      { value: "12", name: "Dec" },
+    ];
+    if (date !== null && !!date) {
+      const dateObj = date.split("-");
+      console.log("dateObj: ", dateObj);
+      let month = monthList.filter((data) => data.value == dateObj[1]);
+      console.log("filter_month: ", month);
+
+      let day = dateObj[2].split(" ");
+      console.log(day);
+      return `${month[0].name} ${day[0]}, ${dateObj[0]}`;
+    }
+  }
+
+  useEffect(() => {
+    if (!!responseStat && !!c_data) {
+      console.log();
+      let perc =
+        (responseStat.data.totalColl * 100) / c_data?.content?.amount_target;
+      setcollPercentage(perc);
+      console.log("percperc: ", perc);
+    }
+  }, [responseStat]);
+
+  function calculateDaysLeft(endDateStr) {
+    // Parse the end date string into a Date object
+    const endDate = new Date(endDateStr);
+
+    // Get the current date
+    const currentDate = new Date();
+
+    // Calculate the difference in milliseconds
+    const timeDifference = endDate - currentDate;
+
+    // Convert the time difference to days
+    const daysLeft = Math.ceil(timeDifference / (1000 * 60 * 60 * 24));
+
+    return daysLeft;
+  }
+
+  // Example usage with the given end date "2024-06-30 00:00"
+  const endDateStr = "2024-06-30T00:00:00";
+  const daysLeft = calculateDaysLeft(endDateStr);
+  
+  console.log(`Days left until ${endDateStr}: ${daysLeft}`);
   return (
     <div className="m-2 sm:m-0">
       <div className="text-center">
@@ -36,7 +101,7 @@ const CampaignProfile = ({ data, generalContent }) => {
           <div className="w-full bg-gray-200 rounded-sm h-2.5 dark:bg-gray-400">
             <div
               className="bg-[#02AA9F] h-2.5 rounded-sm"
-              style={{ width: "45%" }}
+              style={{ width: collPercentage }}
             ></div>
           </div>
 
@@ -44,7 +109,7 @@ const CampaignProfile = ({ data, generalContent }) => {
             <div className="flex flex-col">
               <span className="font-bold text-2xl">
                 {" "}
-                {c_data?.content?.currency} 50,500
+                {c_data?.content?.currency} {responseStat?.data?.totalColl}
               </span>
               <span className="text-xs">
                 received of {c_data?.content?.currency}
@@ -53,12 +118,15 @@ const CampaignProfile = ({ data, generalContent }) => {
             </div>
 
             <div className="flex flex-col">
-              <span className="font-bold text-2xl"> 254</span>
+              <span className="font-bold text-2xl">
+                {" "}
+                {responseStat?.data?.totalDonors}
+              </span>
               <span className="text-xs">donors</span>
             </div>
 
             <div className="flex flex-col">
-              <span className="font-bold text-2xl"> 147</span>
+              <span className="font-bold text-2xl"> {calculateDaysLeft(c_data?.content?.date_end)}</span>
               <span className="text-xs">days to go</span>
             </div>
 
@@ -100,6 +168,11 @@ const CampaignProfile = ({ data, generalContent }) => {
                 </svg>{" "}
                 Remind me
               </button>
+
+              <div className="text-sm text-gray-500">
+                This campaign will end on{" "}
+                {dateFormat(data?.story?.content?.date_end)}
+              </div>
             </div>
           </div>
         </div>
