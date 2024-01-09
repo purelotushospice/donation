debf8054fb7719e2c596ef1bafdf277fd7a050fe
Azwar <azwarnaim97@gmail.com>
2024-01-09 15:14:23 +0800

    @FixCheckout

--- app/payment/success/page.js
+++ app/payment/success/page.js
@@ -1,5 +1,6 @@
 import Breadcrumb from "@/components/breadcrumb";
 import Slider from "@/components/slider";
+import { CheckCircleIcon } from "@heroicons/react/24/outline";
 import { getStoryblokApi } from "@storyblok/react/rsc";
 import StoryblokStory from "@storyblok/react/story";
 
@@ -48,9 +49,9 @@ export default async function Page() {
   ];
   return (
     <div className="">
-      <div className="bg-gray-100">
-        <div className="bg-white p-6  md:mx-auto">
-          <svg
+      <div className="bg-[#E9471F]">
+        <div className="bg-white p-6  md:mx-auto ">
+          {/* <svg
             viewBox="0 0 24 24"
             className="text-green-600 w-16 h-16 mx-auto my-6"
           >
@@ -58,13 +59,21 @@ export default async function Page() {
               fill="currentColor"
               d="M12,0A12,12,0,1,0,24,12,12.014,12.014,0,0,0,12,0Zm6.927,8.2-6.845,9.289a1.011,1.011,0,0,1-1.43.188L5.764,13.769a1,1,0,1,1,1.25-1.562l4.076,3.261,6.227-8.451A1,1,0,1,1,18.927,8.2Z"
             ></path>
-          </svg>
+          </svg> */}
+          <div className="flex justify-center">
+            <CheckCircleIcon className="w-20 h-20 mb-4 text-[#E9471F]" />
+          </div>
+
           <div className="text-center">
             <h3 className="md:text-2xl text-base text-gray-900 font-semibold text-center">
-              Payment Done!
+              Thank you for your generous donation!
             </h3>
-            <p className="text-gray-600 my-2">Thank your for your donation</p>
-            <p> Please check your email for receipt </p>
+            <p className="text-gray-600 my-2 font-bold">
+              {" "}
+              Your contribution means the world to us.
+            </p>
+            {/* <p> Your contribution means the world to us.</p> */}
+            <p> Please check your email for donation confirmation.</p>
             <div className="py-10 text-center">
               <a
                 href="/"
